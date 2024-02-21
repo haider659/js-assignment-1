bf64f5a98e6cf3f4906274c65ef75dbd8e9d5ed7
haider659 <haderkhan659@gmail.com>
2024-02-21 18:57:05 +0500

    js 1

---
+++ app.js
@@ -0,0 +1,115 @@
+// class Assignment
+// chapter 1
+// // Q1
+alert("welcome to my website")
+// // Q2
+alert("Error! Please enter a valid password")
+// // Q3
+alert("WElcome to JS land\nHappy coding")
+// Q4
+alert("welcome to JS land")
+alert("HAppy coding")
+// // Q5
+alert("hello...i can run js through my web browser console's")
+// Q6
+alert("hello")
+// chapter2
+// Q1
+var username = "haider.khan"
+// // Q2
+var myname = "haider khan"
+console.log(myname)
+// // Q3
+var message = "hello world";
+alert("message")
+// Q4
+var firstname = "haider"
+alert(firstname)
+var lastname = "khan"
+alert(lastname)
+var age = "20"
+alert(age) 
+// // Q5
+var food = "pizza \n pizz \n piz \n pi \n p"
+alert(food)
+// // Q6
+var email = "haderkhan659@gmail.com"
+var result = "my email address is "+ email;
+alert(result)
+// // Q7
+var book = "A smarter way to learn javascript";
+var result = "iam trying to learn from book "+book;
+alert(result);
+// Q8
+document.write("yeh! I can write HTML contect through Javacript")
+// Q9
+alert("======╰(*°▽°*)╯🏚🏚🏙╰(*°▽°*)╯=======")
+// chapter 3
+// Q1
+var age = 20;
+var result = "iam "+age+" years old."
+alert(result)
+// // Q2
+var number = 14;
+var result = "you have vivited this site " + number+ " " + "times"
+alert(result)
+// // Q3
+var birthdate = 2003;
+var result = "my birthdate is "+ birthdate +"<br> and i was born in abbotabad"
+
+document.write(result)
+// // Q4
+var customername = "jhon Doe"
+var number = 5;
+var product = "Tshirts"
+var result = customername + "ordered " + " " + number + product + "on XYZ Clothing store";
+document.write(result)
+
+// Home Assignment
+// chapter 1
+// // Q1
+var firstname = "haider"
+alert(firstname)
+var lastname = "khan"
+alert(lastname)
+var email = "haderkhan659@gmail"
+alert(email)
+var Pnumber = 22444555;
+alert(Pnumber)
+var password = 12345;
+alert(password)
+Q2
+alert("You are learning javascript!")
+// // Q3
+alert("Hi iam web and mobile app developnment from jawan Pakistan")
+
+// chapter 2
+// Q1
+var camelcase = "I Lives In Pakistan"
+// Q2
+var insitute = "jawan Pakistan"
+// Q3
+var teamname = "Pakistan"
+alert(teamname)
+// Q4
+var bestman = "Watson"
+
+// chapter 3
+// Q1
+var casQty = ""
+// Q2
+var casQty = 144
+// Q3
+var num = "9"
+// Q5
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+alert(orderTotal)
+// Q6
+var num = 10;
+var num1 = 50;
+var total = num + num1;
+alert(total)
+
+
